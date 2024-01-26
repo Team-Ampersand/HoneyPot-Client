6cbff7d11e3ac7320890f4e9880614709e39e02d
ws-071211 <swhsw1211@gmail.com>
2024-01-26 12:24:30 +0900

    add onBordingPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import GlobalStyles from './public/Globalstyle';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+import Onbording from './components/Onbording'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles/>
-      <Signin/>
+      <Onbording/>
     </div>
   );
 }
---
+++ src/components/Onbording/index.jsx
@@ -0,0 +1,34 @@
+import Monitor from "../../asset/svg/Monitor.svg"
+import * as S from './style';
+
+const Onbording = () => {
+    return(
+        <S.Background>
+            <S.TextButtonContainer>
+                <S.TextContainer>
+                    <S.WelcomeText>
+                        꿀팁이 쌓여 만들어지는 나만의 꿀단지 🍯
+                    </S.WelcomeText>
+                    <S.ExplainText>
+                        꿀단지를 통해 나만의 꿀팁을 공유해봐요.
+                    </S.ExplainText>
+                </S.TextContainer>
+                <S.Button>
+                    시작하기
+                </S.Button>
+            </S.TextButtonContainer>
+            <S.SignupSigninContainer>
+                <S.SignText>
+                    로그인
+                </S.SignText>
+                <S.ContourLine/>
+                <S.SignText>
+                    회원가입
+                </S.SignText>
+            </S.SignupSigninContainer>
+            <S.MonitorImg src={Monitor}/>
+        </S.Background>
+    )
+}
+
+export default Onbording;
