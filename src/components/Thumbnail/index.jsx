179ff3c912d2468701e597c64e014295c878d3d1
Ethen1264 <jxorhks86909763@gmail.com>
2024-02-08 13:52:34 +0900

    add Thumbnail page Publishing

---
+++ src/components/Thumbnail/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as S from './style';
+import Header from '../Header';
+import ThumbnailImg from '../../asset/svg/ThumbnailImg';
+
+const Thumbnail = () => {
+  return (
+    <S.Background>
+      <Header />
+      <S.MainContainer>
+        <S.ThumbnailContainer>
+          <S.TextContainer>
+            <S.Text>썸네일 업로드</S.Text>
+          </S.TextContainer>
+          <S.uploadImgContainer>
+            <ThumbnailImg />
+          </S.uploadImgContainer>
+          <S.ButtonContainer>
+            <S.SubmitButton>등록하기</S.SubmitButton>
+          </S.ButtonContainer>
+        </S.ThumbnailContainer>
+      </S.MainContainer>
+    </S.Background>
+  );
+};
+
+export default Thumbnail;
